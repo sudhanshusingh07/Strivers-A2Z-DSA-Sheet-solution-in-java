66bb7175c4c069de2e145706ee68ecd3ef019069
Sudhanshu Singh <111054333+sudhanshusingh07@users.noreply.github.com>
2024-06-29 22:33:18 +0530

    Create Q21.java

---
+++ Step 1 : Learn the basics/Lec 2 Build-up Logical Thinking/pattern/Q21.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public static void getStarPattern(int n) {
+        
+       for(int i=1;i<=n;i++)
+            {
+                 for(int j=1;j<=n;j++)
+                {
+                if(i==1||i==n||j==1||j==n)
+                {
+                System.out.print('*');
+                }
+                
+                    else
+                    {
+                    System.out.print("");
+                    }
+                
+                System.out.println();
+            }
+                
+    }
+}
