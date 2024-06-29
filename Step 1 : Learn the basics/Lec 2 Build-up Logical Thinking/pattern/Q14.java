44749e30fa76e38be6beb92556f1bd1d435b4547
Sudhanshu Singh <111054333+sudhanshusingh07@users.noreply.github.com>
2024-06-29 22:28:31 +0530

    Create Q14.java

---
+++ Step 1 : Learn the basics/Lec 2 Build-up Logical Thinking/pattern/Q14.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public static void nLetterTriangle(int n) {
+        char k='A';
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(k +" ");
+                k++;
+            }
+            System.out.println();
+            k='A';
+        }
+    }
+}
