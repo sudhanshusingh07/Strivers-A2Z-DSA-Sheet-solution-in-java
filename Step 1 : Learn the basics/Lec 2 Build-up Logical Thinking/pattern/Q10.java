584cc6b246153fb1ab71c4d49fb689b70a92cb13
Sudhanshu Singh <111054333+sudhanshusingh07@users.noreply.github.com>
2024-06-29 21:55:05 +0530

    Create Q10.java

---
+++ Step 1 : Learn the basics/Lec 2 Build-up Logical Thinking/pattern/Q10.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public static void nStarTriangle(int n) {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+        for(int i=1;i<=n-1;i++)
+        {
+            for(int j=1;j<=n-i;j++)
+            {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+
+    }
+}
